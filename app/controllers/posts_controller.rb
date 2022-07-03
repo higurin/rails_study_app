995bf70c34868399c9cd60c9e5bc20d8b7578ccc
樋口梨花 <rika@higuchirikanoMacBook-puro.local>
2022-07-03 16:12:37 +0900

    新規投稿ページを作成

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,6 +2,13 @@ class PostsController < ApplicationController
   def index
     @posts = Post.all
   end
+  def new
+  end
+  def create
+    @post = Post.new(content: params[:content])
+    @post.save
+    redirect_to("/posts/index")
+  end
   def show
     @id = params[:id]
     @post = Post.find_by(id: params[:id])
